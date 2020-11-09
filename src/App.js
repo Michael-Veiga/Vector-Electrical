03be145198e3bf90a09717cfe1ccd7cb72a0d954
Mike V <mvee333@gmail.com>
2020-11-09 12:37:17 -0500

    added touched up logo and created basic routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Route, Switch, BrowserRouter as Router, Link} from 'react-router-dom';
+
+import {Jumbotron, Container, Nav} from 'react-bootstrap';
+
+import vector from './images/vectorTrim.jpg'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <Jumbotron className="pt-2 pb-5 mb-0" style={{backgroundColor: "white"}}>
+            <Container>
+              <img className="jumbo" src={vector} />
+            </Container>
+          </Jumbotron>
+          <Nav className="justify-content-center" activeKey="/" onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
+            <Nav.Item>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={Link} to="/services">Services</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={Link} to="/gallery">Gallery</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            </Nav.Item>
+          </Nav>
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/about' component={About}/>
+              <Route exact path='/services' component={Services}/>
+              <Route exact path='/gallery' component={Gallery}/>
+              <Route exact path='/contact' component={Contact}/>
+            </Switch>
+      </Router>
     </div>
   );
 }
