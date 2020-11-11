5e42992c5a2d82b139616a1379d13f65f95d085a
Mike V <mvee333@gmail.com>
2020-11-11 13:16:47 -0500

    added basic content to home page

---
+++ src/components/HomeContent.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Table, Container} from 'react-bootstrap';
+
+function HomeContent() {
+    return(
+        <Container className="justify-content-center">
+            <h1 className="display-4 pt-2">Welcome to Vector Electrical and Controls</h1>
+            <hr/>
+            <p>We know that you have a choice of electrical contractors in the area and we appreciate your consideration. Our hope is that you'll feel confident in our ability to meet all of your expectations and know that our customers are always our number one focus.</p>
+            <p>Please take a moment to browse through our site to learn more about our company and how we can show you the Vector Electrical and Controls difference. If you have any questions or comments, please feel free to contact us. We look forward to hearing from you soon and thank you for your consideration.</p>
+            <hr/>
+            <h2 className="display-4">Service Areas</h2>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>State</th>
+                        <th>Master's License #</th>
+                        <th>Journeyman's License #</th>
+                    </tr>
+                    </thead>
+                        <tbody>
+                            <tr>
+                                <td>New Hampshire</td>
+                                <td>3768M</td>
+                                <td>5396J</td>
+                            </tr>
+                            <tr>
+                                <td>Massachusetts</td>
+                                <td>9490A</td>
+                                <td>24704E</td>
+                            </tr>
+                        </tbody>
+            </Table>
+        </Container>
+    );
+}
+
+export default HomeContent;
