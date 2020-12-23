2b2149ae5ea07d603ef19053bb6c723a96aece9b
Mike V <mvee333@gmail.com>
2020-12-23 13:03:17 -0500

    altered logo to be better positioned for responsiveness

--- src/components/JumboImg.js
+++ src/components/JumboImg.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import {Jumbotron, Container} from 'react-bootstrap'
+import { Jumbotron, Container } from 'react-bootstrap';
 
 import vector from '../images/vectorSlim.png';
-function JumboImg () {
-        return (
-            <Jumbotron fluid={true} className="jumbo mb-0">
-            <Container className="logoBg" fluid={true}>                     
-              <img className="logo" src={vector} />
-              <h2 className="logoTwo">BRINGING <span className="flicker">POWER</span> TO THE <span className="fast-flicker">PEOPLE</span></h2>            
-            </Container>
-        </Jumbotron>
-            );
+function JumboImg() {
+  return (
+    <Jumbotron fluid={true} className="jumbo mb-0 py-0">
+      <Container className="logoBg" fluid={true}>
+        <div className="centerLogo">
+          <img className="logo" src={vector} />
+          <h2 className="logoTwo">
+            BRINGING <span className="flicker">POWER</span> TO THE{' '}
+            <span className="fast-flicker">PEOPLE</span>
+          </h2>
+        </div>
+      </Container>
+    </Jumbotron>
+  );
 }
 
 export default JumboImg;
-
