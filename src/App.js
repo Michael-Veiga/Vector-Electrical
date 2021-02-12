93c33a0403cc312f6660e27693d61f4df0bee56c
Mike V <mvee333@gmail.com>
2021-02-12 11:53:10 -0500

    altered analytics id

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import JumboImg from './components/JumboImg';
 
 function App() {
   useEffect(() => {
-    ReactGa.initialize(process.env.REACT_APP_ANALYTICS);
+    ReactGa.initialize('G-FQZV9B2G5Y');
 
     // to report page view
     ReactGa.pageview(window.location.pathname);
